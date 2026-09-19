fedb7129efc67100342be32216b658e83d615705
FrancescoAA1 <francescofantin2002@gmail.com>
2026-09-19 23:47:52 +0200

    Initalializing ex2

---
+++ ex2/graph_discovery.py
@@ -0,0 +1,15 @@
+class GraphDiscovery:
+    def __init__(self):
+        pass
+
+    def log_as_dictionary(self, log):
+        pass
+
+    def dependency_graph_inline(self, log):
+        pass
+
+    def read_from_file(filename):
+        pass
+
+    def dependency_graph_file(self, log):
+        pass
